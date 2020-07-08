350a9f132bc57f20ce232d19fb833b69604ced6d
Raysa Aurrecochea <afrodita@Raysas-MacBook-Pro.local>
2020-07-07 22:11:46 -0500

    vue todo list

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,87 @@
+
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="container main" id="app">
+    <h1>{{title}}</h1>
+    <form class="form-inline form" action="">
+      <input class="form-control" type="text" name="todo" v-model="todoText">
+      <button class="btn btn-primary" v-on:click.prevent="createTodo()">Create</button> 
+    </form>
+    <br>
+    <ul>
+      <li v-for="(todo, index) in todos" v-bind:key="index">
+        <input type="checkbox" v-model="todo.isDone" @change="saveToStore()">
+        <label v-bind:class="{'checked': todo.isDone}" for="">{{todo.content}} {{todo.created|date}}</label>
+        <button class="btn btn-danger" @click="deleteTodo(todo)">Delete</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
+moment.locale('en-nz');
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'app',
+  data() {
+    return {
+      title: 'Vuejs Todo App',
+      todoText: '',
+      todos: []
+    }
+  },
+  created() {
+    if (this.isInit) {
+      this.todos = JSON.parse(localStorage.getItem('vues-todos'));
+    }
+  },
+  computed: {
+    isInit() {
+      return localStorage.getItem('vues-todos') !== null;
+    }
+  },
+  filters: {
+    date(value) {
+      return moment(value).calendar();
+    }
+  },
+  methods: {
+    createTodo(msg) {
+      if (this.todoText === '') {
+        return;
+      }
+      this.todos.push({
+        content: this.todoText,
+        created: Date.now(),
+        isDone: false
+      });
+      this.saveToStore();
+      this.todoText = '';
+    },
+    saveToStore() {
+      localStorage.setItem('vues-todos', JSON.stringify(this.todos));
+    },
+    deleteTodo(todo) {
+      this.todos = this.todos.filter((t) => t !== todo);
+      this.saveToStore();
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#app {
+  padding: 50px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.checked {
+  text-decoration: line-through;  
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.main {
+    display: flex;
+    flex-direction: column;
 }
-a {
-  color: #42b983;
+.form {
+    margin: auto;
 }
 </style>
+
